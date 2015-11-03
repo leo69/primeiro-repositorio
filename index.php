e185a927e4ca57670d460461350897f003c7c0a7
leo69 <mexicanoobmx@gmail.com>
2015-11-03 16:45:47 -0200

    alterado hello world

--- index.php
+++ index.php
@@ -4,7 +4,23 @@ require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
 $app->get('/hello/:name' , function ($name) {
-     echo "Hello, " . $name;
+    
+    $inteiro1 = 42; // tipo integer
+    $inteiro2 = 79; // tipointeger
+    $texto = "ola, "; // tipo string
+    $fracao = 2.3; // tipo float
+    
+    $soma = $inteiro1 + $inteiro2;
+    $divisao = $inteiro1 / $fracao;
+    $multi = $inteiro2 * $fracao;
+    
+    $olaMundo = $texto . $name;
+    
+    echo "<p>Soma: " . $soma . "</p>";
+    echo "<p>Divisao: " . $divisao . "</p>";
+    echo "<p>Multiplica: " . $multi . "</p>";    
+    echo "<p>" . $olaMundo . "</p>";
+    echo "Hello, " . $name;
 });
 
 $app->run();
